d3be0994c95e958736dfd3cc042dc3cacab14432
Michel Kreutner <mkreutner@gmail.com>
2026-02-17 21:12:25 +0100

    chore(infra): work on db and api containers - wip

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ ! -f "package.json" ]; then
+  echo "📦 Dossier vide, création du projet api (PHP Laravel)..."
+  composer create-project laravel/laravel .
+fi
+
 # 1. Installation des dépendances si le dossier vendor est absent
 if [ ! -d "vendor" ]; then
   echo "🐘 Installation de Composer..."
@@ -15,11 +20,13 @@ chmod -R 775 storage bootstrap/cache
 # 3. Attente de la base de données avant de lancer les migrations
 echo "⏳ Attente de la base de données..."
 # Cette commande simple vérifie si MySQL répond sur le port 3306
-until nc -z db 3306; do
-  sleep 1
-done
+#until nc -z db 5436; do
+#  sleep 1
+#done
 
 # 4. Migrations (si le projet est vide)
 php artisan migrate --force
+
+php artisan serve --host 0.0.0.0 --port 8000
 
 exec "$@"
